e9daa75d8c26995a70f9a727b7254bd808127a5e
George Goldberg <george@gberg.me>
2016-07-29 19:04:02 +0100

    Introduce ButterKnife, and bind Login views.

--- app/src/main/java/me/gberg/matterdroid/activities/LoginActivity.java
+++ app/src/main/java/me/gberg/matterdroid/activities/LoginActivity.java
@@ -3,11 +3,28 @@ package me.gberg.matterdroid.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.gberg.matterdroid.R;
 import timber.log.Timber;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.co_login_server)
+    TextView serverView;
+
+    @BindView(R.id.co_login_email)
+    TextView emailView;
+
+    @BindView(R.id.co_login_password)
+    TextView passwordView;
+
+    @BindView(R.id.co_login_submit)
+    Button submitView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +34,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.ac_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.co_login_submit)
+    void onSubmitClicked() {
+        Timber.v("Submit Clicked");
     }
 
 }
